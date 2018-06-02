aa9b7d2739a00c257c33731a04c297409d8190d6
akahana-1 <aakahana@gmail.com>
2018-06-03 06:48:54 +0900

    [UPDATE] multiplexing tunerの準備

---
+++ px4/services.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ ! -s /usr/local/etc/mirakurun/channels.yml ]; then
+	cat /usr/local/share/mirakurun/channels.yml > /usr/local/etc/mirakurun/channels.yml
+fi
+if [ ! -s /usr/local/etc/mirakurun/tuners.yml ]; then
+	cat /usr/local/share/mirakurun/tuners.yml > /usr/local/etc/mirakurun/tuners.yml
+fi
+if [ ! -s /usr/local/etc/mirakurun/server.yml ]; then
+	cat /usr/local/share/mirakurun/server.yml > /usr/local/etc/mirakurun/server.yml
+fi
+
+# /usr/bin/mirakurun start
+# /usr/bin/mirakurun log server
+npm start 2>&1 mirakurun-error.log
